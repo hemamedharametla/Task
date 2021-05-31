35c6bbdf95cb766cfdbdbad3a54749286250da2d
hemamedharametla <34718981+hemamedharametla@users.noreply.github.com>
2021-05-31 19:56:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ def counting(item,dict1):
 def csv_file(reader):
     for row in reader:
         row=row.split(',')
-        ipaddress=row[0]
-        url=row[2]
-        error=row[3]
-        counting(ipaddress,ip_dict)
-        counting(url,url_dict)
-        counting(error,error_dict)
+        counting(row[0],ip_dict)
+        counting(row[2],url_dict)
+        counting(row[3],error_dict)
     del ip_dict['IP']
     del url_dict['URL']
     del error_dict['Error Code\n']
@@ -24,7 +21,6 @@ def csv_file(reader):
     print("Lowest reapeted ip address is: ", ip_list_keys[ip_list_values.index(min(ip_list_values))], "count is: ", min(ip_list_values), "times")
 with open("C:\\Users\\Lenovo\\PycharmProjects\\pythonProject\\venv\\Scripts\\weblog.csv","r") as reader:
     csv_file(reader)
-#f=open("C:\\Users\\Lenovo\\PycharmProjects\\pythonProject\\venv\\Scripts\\weblog.csv","r")
     #print("{0}".format(reader.readline().split()))
     #for x in reader:
         #x = x.split(',')

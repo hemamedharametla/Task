34182121af99f53464250607d4e0da93afef0090
hemamedharametla <34718981+hemamedharametla@users.noreply.github.com>
2021-05-31 20:08:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-ip_dict,error_dict,url_dict={},{},{}
-def counting(item,dict1):
+ip_dict,error_dict,url_dict={},{},{}        #Created empty dictionaries     
+def counting(item,dict1):                   #Created function to count of unique ip,error code and url
     if item not in dict1:
         dict1[item]=0
     dict1[item]+=1
-def csv_file(reader):
+def csv_file(reader):                       #Created funtion to do operations
     for row in reader:
         row=row.split(',')
-        counting(row[0],ip_dict)
-        counting(row[2],url_dict)
-        counting(row[3],error_dict)
+        counting(row[0],ip_dict)            #Calling the function
+        counting(row[2],url_dict)           #Calling the function
+        counting(row[3],error_dict)         #Calling the function
     del ip_dict['IP']
     del url_dict['URL']
     del error_dict['Error Code\n']
     print("IP's Count: ",ip_dict)
     print("URL Count: ",url_dict)
     print("Error Code Count: ",error_dict)
-    ip_list_values = list(ip_dict.values())
+    ip_list_values = list(ip_dict.values())     #Converting dictinary into 2 lists for values and keys
     ip_list_keys = list(ip_dict.keys())
     print("Highest reapeted ip address is: ", ip_list_keys[ip_list_values.index(max(ip_list_values))], "count is: ", max(ip_list_values),"times")
     print("Lowest reapeted ip address is: ", ip_list_keys[ip_list_values.index(min(ip_list_values))], "count is: ", min(ip_list_values), "times")
-with open("C:\\Users\\Lenovo\\PycharmProjects\\pythonProject\\venv\\Scripts\\weblog.csv","r") as reader:
+with open("C:\\Users\\Lenovo\\PycharmProjects\\pythonProject\\venv\\Scripts\\weblog.csv","r") as reader:        #File Reading
     csv_file(reader)
 
     
